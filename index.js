9f7306762d290d8e77bf218ec3136c6feb6fac14
friendly-mainframe-3495 <culpepper.jada@gmail.com>
2019-09-08 11:30:25 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,27 @@
+
+function add(a,b) {
+  return(a+b);
+}
+
+function subtract(a,b) {
+  return(a-b);
+}
+
+function multiply(a,b) {
+  return(a*b);
+}
+
+function divide(a,b) {
+  return(a/b);
+} 
+
+var number = 530;
+function inc(number) {
+  return(number);
+}
+
+function dec() {
+  return(n);
+}
+
+
